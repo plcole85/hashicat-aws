17226012f3d65d3d64856208be847bad1dc7b083
plcole85 <35505530+plcole85@users.noreply.github.com>
2022-04-20 11:16:21 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,9 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "perriecronin-cole"
-  acl    = "private"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/plcroniTestOrg/s3-bucket/aws"
+  version = "3.1.0"
+  bucket_prefix = "perriecronin-cole"
+  acl = "private"
   versioning = {
     enabled = true
   }
-
 }
